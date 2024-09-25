200d38558eae85af501792b6b5dd93580f11519b
siwachs <ssiwach.8888@gmail.com>
2024-09-25 10:20:18 +0530

    Layers Depth

--- src/app/board/[boardId]/_components/selectionTools.tsx
+++ src/app/board/[boardId]/_components/selectionTools.tsx
@@ -8,7 +8,7 @@ import ColorPicker from "./colorPicker";
 import { Button } from "@/components/ui/button";
 import Hint from "@/components/hint";
 
-import { Trash2 } from "lucide-react";
+import { Trash2, BringToFront, SendToBack } from "lucide-react";
 
 const SelectionTools: React.FC<{
   camera: Camera;
@@ -17,6 +17,45 @@ const SelectionTools: React.FC<{
   const selection = useSelf((me) => me.presence.selection);
 
   const selectionBounds = useSelectionBound();
+
+  const moveToBack = useMutation(
+    ({ storage }) => {
+      const liveLayerIds = storage.get("layerIds");
+      const indices: number[] = [];
+
+      const arr = liveLayerIds.toArray();
+
+      for (let i = 0; i < arr.length; i++) {
+        if (selection.includes(arr[i])) indices.push(i);
+      }
+
+      for (let i = 0; i < indices.length; i++) {
+        liveLayerIds.move(indices[i], i);
+      }
+    },
+    [selection],
+  );
+
+  const moveToFront = useMutation(
+    ({ storage }) => {
+      const liveLayerIds = storage.get("layerIds");
+      const indices: number[] = [];
+
+      const arr = liveLayerIds.toArray();
+
+      for (let i = 0; i < arr.length; i++) {
+        if (selection.includes(arr[i])) indices.push(i);
+      }
+
+      for (let i = indices.length - 1; i >= 0; i--) {
+        liveLayerIds.move(
+          indices[i],
+          arr.length - 1 - (indices.length - 1 - i),
+        );
+      }
+    },
+    [selection],
+  );
 
   const setFill = useMutation(
     ({ storage }, fill: Color) => {
@@ -46,7 +85,17 @@ const SelectionTools: React.FC<{
       <ColorPicker onChange={setFill} />
 
       <div className="flex flex-col gap-y-0.5">
-        <Hint></Hint>
+        <Hint label="Bring to front">
+          <Button onClick={moveToFront} variant="board" size="icon">
+            <BringToFront />
+          </Button>
+        </Hint>
+
+        <Hint label="Send to back">
+          <Button onClick={moveToBack} variant="board" size="icon">
+            <SendToBack />
+          </Button>
+        </Hint>
       </div>
 
       <div className="ml-2 flex items-center border-l border-neutral-200 pl-2">
